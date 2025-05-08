dd8efd6a3d1dae527ceace95ea774c4593c93b3e
David León <daviiidleongutierrez@gmail.com>
2025-05-08 08:48:55 +0200

    Configuration pages

--- DlFinanceManager/src/app/page/dashboard/dashboard.page.ts
+++ DlFinanceManager/src/app/page/dashboard/dashboard.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import {HeaderComponent} from "../../component/header/header.component";
 import {SideMenuComponent} from "../../component/side-menu/side-menu.component";
 
@@ -10,7 +10,7 @@ import {SideMenuComponent} from "../../component/side-menu/side-menu.component";
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, SideMenuComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, SideMenuComponent, IonIcon]
 })
 export class DashboardPage implements OnInit {
 
