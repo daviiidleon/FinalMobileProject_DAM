c3eaec1b52274197abd67ffc256aa3f96bb78b4c
David León <daviiidleongutierrez@gmail.com>
2025-05-14 10:22:51 +0200

    Configuration transacciones page

--- DlFinanceManager/src/app/page/transacciones/transacciones.page.ts
+++ DlFinanceManager/src/app/page/transacciones/transacciones.page.ts
@@ -1,22 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
-import {HeaderComponent} from "../../component/header/header.component";
-import {SideMenuComponent} from "../../component/side-menu/side-menu.component";
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { HeaderComponent } from "../../component/header/header.component";
+import { SideMenuComponent } from "../../component/side-menu/side-menu.component";
 
 @Component({
   selector: 'app-transacciones',
   templateUrl: './transacciones.page.html',
   styleUrls: ['./transacciones.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, HeaderComponent, SideMenuComponent, IonIcon, IonButton]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    SideMenuComponent,
+    IonIcon,
+    IonButton,
+    IonCol,
+    IonRow,
+    IonGrid
+  ]
 })
 export class TransaccionesPage implements OnInit {
+
+  // ✅ Cambiado: arreglo de transacciones inicializado
+  transacciones: any[] = [
+    {
+      fecha: '14/05/2025',
+      cuenta: 'Cuenta Corriente',
+      cantidad: '€120.00',
+      categoria: 'Alimentación',
+      descripcion: 'Supermercado'
+    },
+    {
+      fecha: '13/05/2025',
+      cuenta: 'Tarjeta Crédito',
+      cantidad: '€35.00',
+      categoria: 'Transporte',
+      descripcion: 'Gasolina'
+    },
+    {
+      fecha: '12/05/2025',
+      cuenta: 'Efectivo',
+      cantidad: '€20.00',
+      categoria: 'Ocio',
+      descripcion: 'Cine'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    // Aquí podrías cargar transacciones desde un servicio en el futuro
   }
 
 }
